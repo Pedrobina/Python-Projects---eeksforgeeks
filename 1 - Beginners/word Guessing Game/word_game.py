c934a94baafa17dfdb30be384bd183f808c15af7
Pedro Miranda <pedro17rb@hotmail.com>
2026-06-19 16:54:21 -0300

    update on the find characters of the name of the pokemon.

--- 1 - Beginners/word Guessing Game/word_game.py
+++ 1 - Beginners/word Guessing Game/word_game.py
@@ -1,9 +1,12 @@
 import random
 
-words = ['pikachu', 'buterfree', 'psyduck', 'togepi', 'blastoise', 'venussaur', 'bubassaur', 'charmander', 'squirtle','onix', 'geodude', 'zubat','totodille', 'vulpix', 'Mr. Mine', 'meowth', 'arbok','weezing']
-word = random.choice(words)
+pokemons = ['pikachu', 'buterfree', 'psyduck', 'togepi', 'blastoise', 'venussaur', 'bubassaur',
+          'charmander', 'squirtle','onix', 'geodude', 'zubat','totodille', 'vulpix', 'voltorb', 'meowth', 'arbok','weezing']
+pokemon = random.choice(pokemons)
 guesses = []
 turns = 12
+for p in pokemon:
+    guesses.append('_')
 
 print('Please guess the Pokemon:\n')
 while turns > 0:
@@ -12,8 +15,12 @@ while turns > 0:
     print('Tentatives ', turns)
     print('Please enter a letter:')
     letter = str(input())
-    for l in word:
-        if l == letter:
-            guesses.append(l)
+    for x in pokemon:
+        if x == letter:
+            i = pokemon.index(x)
+            guesses[i] = letter  
+
+    if guesses.index('_') < 0:
+        print('Your Pokemon was founded')
     turns-=1
 
