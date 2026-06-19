b2fe139363e8fe7769d84a40881e282e021a4b8b
Pedro Miranda <pedro17rb@hotmail.com>
2026-06-19 12:23:59 -0300

    New incrementatioon high and low choices.

--- 1 - Beginners/number_guessing_game/game.py
+++ 1 - Beginners/number_guessing_game/game.py
@@ -1,15 +1,26 @@
 import random
 
-user_choice = 0
+
 computer_choice  = random.randint(1, 10)
 chances = 5
+print('please choose a number between 1 and 10')
+user_choice = int(input())
 
 while chances != 0:
-    if user_choice != computer_choice:
-        print('Please choose a number between 1 and 10')
-        print('You have ',chances,' more chances.')
+    if (user_choice < 1 or user_choice > 10) and user_choice != None:
+        print('Invalid number, please choose a number between 1 and 10')
         user_choice = int(input())
-        chances -= 1
     else:
-        print('Congrats you guessed rigth! The computer number is: ', computer_choice)
-        break
+        if user_choice > computer_choice:
+            print('Thats to high, please try again.')
+            print('You have ',chances,' more chances. \n')
+            user_choice = int(input())
+            chances -= 1
+        elif (user_choice < computer_choice):
+            print('Thats to low, please try again.')
+            print('You have ',chances,' more chances. \n')
+            user_choice = int(input())
+            chances -= 1
+        else:
+            print('Congrats you guessed rigth! The computer number is: ', computer_choice, '\n')
+            break
